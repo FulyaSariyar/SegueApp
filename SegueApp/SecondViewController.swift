ce44c669aca8da0930670c9ddf2f2d60a7a8b5fd
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-15 18:32:24 +0300

    init

---
+++ SegueApp/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  SegueApp
+//
+//  Created by Fulya Sarıyar on 15.06.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var myLabelSecond: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Fulya Sarıyar on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
